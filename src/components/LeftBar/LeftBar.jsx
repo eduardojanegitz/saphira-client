76db7bb7c973f24ad5e4e242da132272154c1f3f
Eduardo Alves <edu0alves00@gmail.com>
2025-04-21 20:36:56 -0300

    feat: responsive leftbar

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./LeftBar.css"
 
 const LeftBar = () => {
   return (
-    <div className='leftBar'>
+    <div className="leftBar">
       <div className="menuIcons">
-        <a href="/"></a>
+        <a href="/" className="menuIcon">
+          <img src="/general/logo.png" alt="" className="logo"/>
+        </a>
+        <a href="/" className="menuIcon">
+          <img src="/general/home.svg" alt="" />
+        </a>
+        <a href="/" className="menuIcon">
+          <img src="/general/create.svg" alt="" />
+        </a>
+        <a href="/" className="menuIcon">
+          <img src="/general/updates.svg" alt="" />
+        </a>
+        <a href="/" className="menuIcon">
+          <img src="/general/messages.svg" alt="" />
+        </a>
       </div>
+      <a href="/" className="menuIcon">
+        <img src="/general/messages.svg" alt="" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default LeftBar
+export default LeftBar;
